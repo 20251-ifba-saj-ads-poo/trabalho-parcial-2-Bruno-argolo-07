fdc4b81b77d928204f93fb3b21777f18a58c76ac
Bruno-argolo-07 <163079213+Bruno-argolo-07@users.noreply.github.com>
2025-08-25 19:05:10 -0300

    Create ItemPedido.java

---
+++ ItemPedido.java
@@ -0,0 +1,20 @@
+public class ItemPedido {
+    private ItemCardapio item;
+    private int quantidade;
+
+    public ItemPedido(ItemCardapio item, int quantidade) {
+        this.item = item;
+        this.quantidade = quantidade;
+    }
+
+    public double getSubtotal() {
+        return item.getPreco() * quantidade;
+    }
+
+
+    public ItemCardapio getItem() { return item; }
+    public void setItem(ItemCardapio item) { this.item = item; }
+
+    public int getQuantidade() { return quantidade; }
+    public void setQuantidade(int quantidade) { this.quantidade = quantidade; }
+}
