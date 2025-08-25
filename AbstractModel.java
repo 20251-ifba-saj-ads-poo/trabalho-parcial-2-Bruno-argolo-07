4cab30a9581c706e8624506272ee1495cf37a406
Bruno-argolo-07 <163079213+Bruno-argolo-07@users.noreply.github.com>
2025-08-25 19:14:05 -0300

    Create AbstractModel.java

---
+++ AbstractModel.java
@@ -0,0 +1,25 @@
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public abstract class AbstractModel {
+    private UUID id;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+    private String createdBy;
+    private String updatedBy;
+
+    public UUID getId() { return id; }
+    public void setId(UUID id) { this.id = id; }
+
+    public LocalDateTime getCreatedAt() { return createdAt; }
+    public void setCreatedAt(LocalDateTime createdAt) { this.createdAt = createdAt; }
+
+    public LocalDateTime getUpdatedAt() { return updatedAt; }
+    public void setUpdatedAt(LocalDateTime updatedAt) { this.updatedAt = updatedAt; }
+
+    public String getCreatedBy() { return createdBy; }
+    public void setCreatedBy(String createdBy) { this.createdBy = createdBy; }
+
+    public String getUpdatedBy() { return updatedBy; }
+    public void setUpdatedBy(String updatedBy) { this.updatedBy = updatedBy; }
+}
